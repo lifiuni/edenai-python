ce1ecb8484f0b4b86bb856bc66d06dae2b8f0b63
Eden AI <67952486+edenai@users.noreply.github.com>
2021-07-30 11:05:40 +0200

    Create edenai-face-detection.py

---
+++ api_requests/edenai-face-detection.py
@@ -0,0 +1,21 @@
+import requests
+
+# You can find the documentation here : https://api.edenai.run/v1/redoc/#section/How-it-works
+
+#Enter your API Token
+headers = {  'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
+
+# Select API
+url = 'https://api.edenai.run/v1/pretrained/vision/face_detection'
+
+# Select providers
+payload = {'providers': '[\'google_cloud\', \'microsoft\', \'aws\']'}
+
+# Select file to test
+files = [  ('files', open('Picture/example.jpg','rb'))]
+
+# Request to Eden AI
+response = requests.request("POST", url, headers=headers, data = payload, files = files)
+
+# Print result
+print(response.text.encode('utf8'))
