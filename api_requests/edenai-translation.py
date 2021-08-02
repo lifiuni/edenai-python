df96e163dba73fcf57e355af26b9290e3fce04f0
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:48:49 +0200

    Update edenai-translation.py

--- api_requests/edenai-translation.py
+++ api_requests/edenai-translation.py
@@ -9,7 +9,7 @@ headers = {'Authorization': 'Bearer API Key'}
 url = 'https://api.edenai.run/v1/pretrained/text/automatic_translation'
 
 # Select providers, and text to translate, and source / target languages
-payload = {'providers': '[\'ibm\', \'microsoft\', \'aws\', \'google_cloud\']','text_to_translate':'how are you today', 'source_language': 'en','target_language': 'fr'}
+payload = {'providers': '[\'ibm\', \'microsoft\', \'aws\', \'google\']','text_to_translate':'how are you today', 'source_language': 'en','target_language': 'fr'}
 
 # Request to Eden AI
 response = requests.request("POST", url, headers=headers, data = payload, files = files)
