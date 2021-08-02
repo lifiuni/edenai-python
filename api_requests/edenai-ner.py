ca33da299049f296c154f1780394d179d31c8bff
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:46:17 +0200

    Update edenai-ner.py

--- api_requests/edenai-ner.py
+++ api_requests/edenai-ner.py
@@ -9,7 +9,7 @@ headers = {'Authorization': 'Bearer your API Key'}
 url = 'https://api.edenai.run/v1/pretrained/text/named_entity_recognition'
 
 # Select providers, and text to analyze
-payload = {'providers': '[\'google_cloud\', \'microsoft\', \'aws\', \'ibm\']','text':'I am angry today', 'entities_to_find': '','language': 'en-US'}
+payload = {'providers': '[\'google\', \'microsoft\', \'aws\', \'ibm\']','text':'I am angry today', 'entities_to_find': '','language': 'en-US'}
 
 # Request to Eden AI
 response = requests.request("POST", url, headers=headers, data = payload, files = files)
