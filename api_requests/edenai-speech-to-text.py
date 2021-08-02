cb89bf9bb173547829d29116d89c17eb82cdd4b9
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:48:01 +0200

    Update edenai-speech-to-text.py

--- api_requests/edenai-speech-to-text.py
+++ api_requests/edenai-speech-to-text.py
@@ -9,7 +9,7 @@ headers = {'Authorization': 'Bearer your API Key'}
 url = 'https://api.edenai.run/v1/pretrained/audio/speech_recognition'
 
 # Select providers, and audio to transcribe
-payload = {'providers':'[\'ibm\', \'microsoft\', \'aws\', \'google_cloud\']','text_to_find':'Bonjour, je suis Martin','language':'fr-FR'}
+payload = {'providers':'[\'ibm\', \'microsoft\', \'aws\', \'google\']','text_to_find':'Bonjour, je suis Martin','language':'fr-FR'}
 
 # Select file to test         
 files = {'files': open('Music/0wr69-jf4nc.wav','rb')}
