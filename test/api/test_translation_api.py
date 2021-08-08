e544add5abad941ef9e9099261e509d7e4c22370
Dmitrii Kurlov <dmitriik@tutanota.com>
2021-08-09 02:51:36 +0700

    Adding Translation API

--- test/api/test_translation_api.py
+++ test/api/test_translation_api.py
@@ -31,3 +31,31 @@ def api_wrong():
 )
 def test_endpoints(api: Translation, test_input, expected):
     assert api.get_endpoint_url(test_input) == expected
+
+
+@pytest.mark.skip_internet_tests
+def test_translation(api: Translation):
+    result = api.translation("Text to translate", "en-US", "fr-FR", ["amazon"])
+
+    assert len(result["Amazon Web Services"]) > 0
+    assert isinstance(result["Amazon Web Services"], str)
+
+
+@pytest.mark.skip_internet_tests
+def test_language_detection(api: Translation):
+    result = api.language_detection("Text to detection", "en", ["amazon"])
+
+    assert len(result["Amazon Web Services"][0]) > 0
+    assert len(result["Amazon Web Services"][1]) > 0
+
+
+@pytest.mark.skip_internet_tests
+def test_translation_error(api_wrong: Translation):
+    result = api_wrong.translation("Text to translate", "en", "fr", ["amazon"])
+    assert "errors" in result
+
+
+@pytest.mark.skip_internet_tests
+def test_language_detection_error(api_wrong: Translation):
+    result = api_wrong.language_detection("Text to detection", "en", ["amazon"])
+    assert "errors" in result
