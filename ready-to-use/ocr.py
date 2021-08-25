7c10d5154ec0024e5d1c5879dc04826118884548
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-25 15:19:25 +0200

    Create ocr.py

---
+++ ready-to-use/ocr.py
@@ -0,0 +1,14 @@
+pip install edenai
+
+from edenai import OCR
+
+# Get your API key here: https://app.edenai.run/admin/account
+ocr_apis = OCR("Your_API_key")
+
+result = ocr_apis.basic(
+# Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/OCR
+   providers=["google","amazon"],
+   language="en-US",
+   file="your_image.png")
+
+print(result)
