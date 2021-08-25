8e52905892bc89a537d4a01c83e4d346995ee698
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-25 16:10:56 +0200

    Update language-detection.py

--- ready-to-use/language-detection.py
+++ ready-to-use/language-detection.py
@@ -7,7 +7,7 @@ translation_apis = Translation("Your_API_key")
 
 result = translation_apis.language_detection(
 # Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Language%20Detection
-   text="Your text written in an unknown language"
+   text="Your text written in an unknown language",
    providers=["amazon", "ibm"])
 
 print(result)
