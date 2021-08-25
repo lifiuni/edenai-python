52146d8cfc4c12fbb58c283ec1b0ff099e77d090
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-25 15:47:01 +0200

    Create machine-translation.py

---
+++ ready-to-use/machine-translation.py
@@ -0,0 +1,15 @@
+pip install edenai
+
+from edenai import Translation
+
+# Get your API key here: https://app.edenai.run/admin/account
+translation_apis = Translation("Your_API_key")
+
+result = translation_apis.translation(
+# Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Automatic%20Translation
+   text_to_translate= "The text you want to translate",
+   providers=["amazon", "ibm"],
+   source_language="en-US",
+   target_language="fr-FR")
+
+print(result)
