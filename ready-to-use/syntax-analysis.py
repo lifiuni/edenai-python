46818466568c843d064b0cd50eb0209596628fb9
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-25 18:15:49 +0200

    Eden AI with Python: Syntax analysis example

---
+++ ready-to-use/syntax-analysis.py
@@ -0,0 +1,14 @@
+pip install edenai
+
+from edenai import Text
+
+# Get your API key here: https://app.edenai.run/admin/account
+text_apis = Text("Your_API_key")
+
+result = text_apis.syntax_analysis(
+# Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Language%20Detection
+    text="Your text",
+    providers=["lettria", "microsoft"],
+    language="en-US")
+
+print(result) 
