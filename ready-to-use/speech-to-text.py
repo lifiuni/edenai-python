9ecd5b5586fde6388c6b7d9e8ae90c75f4d50f1e
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-25 18:40:53 +0200

    Eden AI with Python: Speech-to-Text example

---
+++ ready-to-use/speech-to-text.py
@@ -0,0 +1,14 @@
+pip install edenai
+
+from edenai import Audio
+
+# Get your API key here: https://app.edenai.run/admin/account
+audio_apis = Audio("Your_API_key")
+
+result = audio_apis.speech_to_text(
+# Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Speech%20Recognition
+    file="your_audio.wav",
+    providers=["amazon", "ibm"],
+    language="en-US")
+
+print(result) 
