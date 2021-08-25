e8bcd4fc8e55df33d6d8006407d29524ebb296f0
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-25 16:05:43 +0200

    Create language-detection.py

---
+++ ready-to-use/language-detection.py
@@ -0,0 +1,13 @@
+pip install edenai
+
+from edenai import Translation
+
+# Get your API key here: https://app.edenai.run/admin/account
+translation_apis = Translation("Your_API_key")
+
+result = translation_apis.language_detection(
+# Available providers and languages here: https://api.edenai.run/v1/redoc/#operation/Language%20Detection
+   text="Your text written in an unknown language"
+   providers=["amazon", "ibm"])
+
+print(result)
