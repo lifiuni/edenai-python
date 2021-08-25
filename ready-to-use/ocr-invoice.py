8a05fe800ce74f0bfd538ef69ca559aa08df7214
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-25 15:40:58 +0200

    Create ocr-invoice.py

---
+++ ready-to-use/ocr-invoice.py
@@ -0,0 +1,14 @@
+pip install edenai
+
+from edenai import OCR
+
+# Get your API key here: https://app.edenai.run/admin/account
+ocr_apis = OCR("Your_API_key")
+
+result = ocr_apis.invoice(
+# Available providers, languages and formats here: https://api.edenai.run/v1/redoc/#operation/OCR%20Invoice
+   providers=["microsoft", "mindee"],
+   language="en-US",
+   file="your_invoice.pdf")
+
+print(result)
