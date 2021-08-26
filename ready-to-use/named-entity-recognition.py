6aca5591d3cba90da36dfc024bc9c20acc8f34c1
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-26 11:40:06 +0200

    Eden AI with Python: Named Entity Recognition example
    Named Entity Recognition (NER - also called entity identification or entity extraction) is an information extraction technique that automatically identifies named entities in a text and classifies them into predefined categories.

--- ready-to-use/named-entity-recognition.py
+++ ready-to-use/named-entity-recognition.py
@@ -1,6 +1,6 @@
 # Named Entity Recognition (NER - also called entity identification or entity extraction) is an information extraction technique that automatically identifies named entities in a text and classifies them into predefined categories.
 
-pip install edenai
+# pip install edenai
 
 from edenai import Text
 
