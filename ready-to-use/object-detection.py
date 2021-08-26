2b6d5e32c8a2c8683445b14e6d1afce74253b7bf
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-26 09:47:06 +0200

    Eden AI with Python: Object detection example
    Object detection is a computer vision technology which allows to detect common objects on images.

--- ready-to-use/object-detection.py
+++ ready-to-use/object-detection.py
@@ -7,8 +7,8 @@ from edenai import Vision
 # Get your API key here: https://app.edenai.run/admin/account
 vision_apis = Vision("Your_API_key")
 
-result = vision_apis.face_detection(
-# Available providers, languages and formats here: https://api.edenai.run/v1/redoc/#operation/Face%20Detection
+result = vision_apis.object_detection(
+# Available providers, languages and formats here: https://api.edenai.run/v1/redoc/#operation/Object%20Detection
    providers=["google","amazon"],
    file="your_image.png")
 
